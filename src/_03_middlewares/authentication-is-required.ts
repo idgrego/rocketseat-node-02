75957c1418f51cee4ad0559c4649aad6d49e78f4
Ivo Gregorio <idgrego@msn.com>
2025-09-02 16:41:41 -0300

    ajuste no método de verificação de autenticação para já retornar o código do usuário dentro do request (no cabeçalho)

--- src/_03_middlewares/authentication-is-required.ts
+++ src/_03_middlewares/authentication-is-required.ts
@@ -1,7 +1,15 @@
-import type { FastifyRequest, FastifyReply } from "fastify"
+import { type FastifyRequest, type FastifyReply, fastify } from "fastify"
+import { dbContext } from "../_01_infra/db-context.js"
+import { User } from "../_02_models/user.model.js"
 
 export async function authenticationisRequired(request: FastifyRequest, response: FastifyReply)  {
-    if (!request.cookies.sessionId) {
+    if (!request.cookies.sessionId)
         return response.status(401).send({ error: 'Unauthorized' })
-    }
+
+    const sessionId = request.cookies.sessionId
+    const user = await dbContext(User.table).where('sessionId', sessionId).select().first()
+    if (!user)
+        return response.status(401).send({ error: 'Unauthorized' })
+    
+    request.headers['userId'] = user.id;
 }
